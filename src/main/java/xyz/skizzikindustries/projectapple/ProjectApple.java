bd0943f389e8225efe232983e05653558dda6c77
uSkizzik <58179002+uSkizzik@users.noreply.github.com>
2021-05-17 22:58:01 +0300

    Tiny Changes

--- src/main/java/xyz/skizzikindustries/projectapple/ProjectApple.java
+++ src/main/java/xyz/skizzikindustries/projectapple/ProjectApple.java
@@ -2,12 +2,16 @@ package xyz.skizzikindustries.projectapple;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import xyz.skizzikindustries.projectapple.init.Register;
 
 @Mod(ProjectApple.MOD_ID)
 public class ProjectApple
 {
     public static final String MOD_ID = "skizzik";
+
+    public static final Logger LOGGER = LogManager.getLogger();
 
     public ProjectApple() {
         Register.register();
