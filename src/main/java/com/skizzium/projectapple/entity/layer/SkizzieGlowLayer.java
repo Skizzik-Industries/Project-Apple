d792f499bcb55162e2f7a6f6453cfa8bbd8ddb49
uSkizzik <58179002+uSkizzik@users.noreply.github.com>
2021-09-09 20:23:03 +0300

    Spookzies

--- src/main/java/com/skizzium/projectapple/entity/layer/SkizzieGlowLayer.java
+++ src/main/java/com/skizzium/projectapple/entity/layer/SkizzieGlowLayer.java
@@ -2,6 +2,7 @@ package com.skizzium.projectapple.entity.layer;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.skizzium.projectapple.ProjectApple;
 import com.skizzium.projectapple.entity.CorruptedSkizzie;
 import com.skizzium.projectapple.entity.FriendlySkizzie;
 import com.skizzium.projectapple.entity.FriendlyWitchSkizzie;
@@ -20,24 +21,34 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 @OnlyIn(Dist.CLIENT)
 public class SkizzieGlowLayer<T extends LivingEntity> extends RenderLayer<T, SkizzieModel<T>> {
     private static final ResourceLocation FRIENDLY_SKIZZIE_GLOW = new ResourceLocation("skizzik:textures/entity/friendly_skizzie/friendly_skizzie_glow.png");
-    private static final ResourceLocation FRIENDLY_WITCH_SKIZZIE_GLOW = new ResourceLocation("skizzik:textures/entity/friendly_skizzie/friendly_witch_skizzie_glow.png");
     private static final ResourceLocation SKIZZIE_GLOW = new ResourceLocation("skizzik:textures/entity/skizzie/skizzie_glow.png");
     private static final ResourceLocation KABOOM_SKIZZIE_GLOW = new ResourceLocation("skizzik:textures/entity/skizzie/kaboom_skizzie_glow.png");
     private static final ResourceLocation CORRUPTED_SKIZZIE_GLOW = new ResourceLocation("skizzik:textures/entity/skizzie/corrupted_skizzie_glow.png");
+
+    private static final ResourceLocation FRIENDLY_SPOOKZIE_GLOW = new ResourceLocation("skizzik:textures/entity/holidays/spooktober/friendly_spookzie/friendly_spookzie_glow.png");
+    private static final ResourceLocation SPOOKZIE_GLOW = new ResourceLocation("skizzik:textures/entity/holidays/spooktober/spookzie/spookzie_glow.png");
+    private static final ResourceLocation KABOOM_SPOOKZIE_GLOW = new ResourceLocation("skizzik:textures/entity/holidays/spooktober/spookzie/kaboom_spookzie_glow.png");
+    private static final ResourceLocation CORRUPTED_SPOOKZIE_GLOW = new ResourceLocation("skizzik:textures/entity/holidays/spooktober/spookzie/corrupted_spookzie_glow.png");
 
     public SkizzieGlowLayer(RenderLayerParent<T, SkizzieModel<T>> renderer) {
         super(renderer);
     }
 
+    private ResourceLocation getTexture(T entity) {
+        if (ProjectApple.holiday == 1) {
+            return entity instanceof FriendlySkizzie ? FRIENDLY_SPOOKZIE_GLOW :
+                   entity instanceof KaboomSkizzie ? KABOOM_SPOOKZIE_GLOW :
+                   entity instanceof CorruptedSkizzie ? CORRUPTED_SPOOKZIE_GLOW : SPOOKZIE_GLOW;
+        }
+
+        return entity instanceof FriendlySkizzie ? FRIENDLY_SKIZZIE_GLOW :
+               entity instanceof KaboomSkizzie ? KABOOM_SKIZZIE_GLOW :
+               entity instanceof CorruptedSkizzie ? CORRUPTED_SKIZZIE_GLOW : SKIZZIE_GLOW;
+    }
+
     @Override
     public void render(PoseStack pose, MultiBufferSource buffer, int light, T entity, float limbSwing, float limbSwingAmount, float partialTicks, float age, float headYaw, float headPitch) {
-        ResourceLocation texture = entity instanceof FriendlyWitchSkizzie ? FRIENDLY_WITCH_SKIZZIE_GLOW :
-                                    entity instanceof FriendlySkizzie ? FRIENDLY_SKIZZIE_GLOW :
-                                        entity instanceof KaboomSkizzie ? KABOOM_SKIZZIE_GLOW :
-                                            entity instanceof CorruptedSkizzie ? CORRUPTED_SKIZZIE_GLOW :
-                                                SKIZZIE_GLOW;
-
-        VertexConsumer vertex = buffer.getBuffer(RenderType.eyes(texture));
+        VertexConsumer vertex = buffer.getBuffer(RenderType.eyes(getTexture(entity)));
         this.getParentModel().renderToBuffer(pose, vertex, 15728640, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
     }
 }
--- src/main/java/com/skizzium/projectapple/entity/renderer/SkizzieRenderer.java
+++ src/main/java/com/skizzium/projectapple/entity/renderer/SkizzieRenderer.java
@@ -1,5 +1,6 @@
 package com.skizzium.projectapple.entity.renderer;
 
+import com.skizzium.projectapple.ProjectApple;
 import com.skizzium.projectapple.entity.*;
 import com.skizzium.projectapple.entity.layer.SkizzieGlowLayer;
 import com.skizzium.projectapple.entity.model.SkizzieModel;
@@ -18,12 +19,23 @@ public class SkizzieRenderer extends MobRenderer<Mob, SkizzieModel<Mob>> {
     private static final ResourceLocation KABOOM_SKIZZIE_LOCATION = new ResourceLocation("skizzik:textures/entity/skizzie/kaboom_skizzie.png");
     private static final ResourceLocation CORRUPTED_SKIZZIE_LOCATION = new ResourceLocation("skizzik:textures/entity/skizzie/corrupted_skizzie.png");
 
+    private static final ResourceLocation FRIENDLY_SPOOKZIE_LOCATION = new ResourceLocation("skizzik:textures/entity/holidays/spooktober/friendly_spookzie/friendly_spookzie.png");
+    private static final ResourceLocation SPOOKZIE_LOCATION = new ResourceLocation("skizzik:textures/entity/holidays/spooktober/spookzie/spookzie.png");
+    private static final ResourceLocation KABOOM_SPOOKZIE_LOCATION = new ResourceLocation("skizzik:textures/entity/holidays/spooktober/spookzie/kaboom_spookzie.png");
+    private static final ResourceLocation CORRUPTED_SPOOKZIE_LOCATION = new ResourceLocation("skizzik:textures/entity/holidays/spooktober/spookzie/corrupted_spookzie.png");
+
     public SkizzieRenderer(EntityRendererProvider.Context renderer) {
         super(renderer, new SkizzieModel<>(renderer.bakeLayer(PA_ModelLayers.SKIZZIE_LAYER)), 0.45F);
         this.addLayer(new SkizzieGlowLayer<>(this));
     }
 
     public ResourceLocation getTextureLocation(Mob entity) {
+        if (ProjectApple.holiday == 1) {
+            return entity instanceof FriendlySkizzie ? FRIENDLY_SPOOKZIE_LOCATION :
+                   entity instanceof KaboomSkizzie ? KABOOM_SPOOKZIE_LOCATION :
+                   entity instanceof CorruptedSkizzie ? CORRUPTED_SPOOKZIE_LOCATION : SPOOKZIE_LOCATION;
+        }
+
         return entity instanceof FriendlySkizzie ? FRIENDLY_SKIZZIE_LOCATION :
                entity instanceof KaboomSkizzie ? KABOOM_SKIZZIE_LOCATION :
                entity instanceof CorruptedSkizzie ? CORRUPTED_SKIZZIE_LOCATION : SKIZZIE_LOCATION;
