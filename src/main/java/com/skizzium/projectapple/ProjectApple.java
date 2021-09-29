083d9ae326a9b9bac13f26aa48345104b0f3e97f
uSkizzik <58179002+uSkizzik@users.noreply.github.com>
2021-09-29 12:15:46 +0300

    Shadowing & Shit

--- src/main/java/com/skizzium/projectapple/ProjectApple.java
+++ src/main/java/com/skizzium/projectapple/ProjectApple.java
@@ -9,6 +9,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -18,6 +19,7 @@ import net.minecraftforge.fml.loading.FMLEnvironment;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import software.bernie.geckolib3.GeckoLib;
+import software.bernie.geckolib3.resource.ResourceListener;
 
 import java.util.List;
 import java.util.Map;
@@ -41,14 +43,12 @@ public class ProjectApple {
         holiday = checkForHolidays();
         
         PA_Registry.register();
-
-        final IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
-        if (FMLEnvironment.dist == Dist.CLIENT)
-            GeckoLib.initialize();
+        
+        GeckoLib.hasInitialized = true;
+        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ResourceListener::registerReloadListener);
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, PA_Config.commonSpec);
 
         MinecraftForge.EVENT_BUS.register(this);
-        IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
     }
 
     private static int checkForHolidays() {
