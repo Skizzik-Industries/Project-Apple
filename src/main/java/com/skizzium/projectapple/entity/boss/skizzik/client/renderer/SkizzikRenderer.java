33af3904b527767be2023a47cf8a0d5e83e07cc3
uSkizzik <58179002+uSkizzik@users.noreply.github.com>
2021-10-05 20:40:37 +0300

    Glowing Shit

--- src/main/java/com/skizzium/projectapple/entity/boss/skizzik/client/renderer/SkizzikRenderer.java
+++ src/main/java/com/skizzium/projectapple/entity/boss/skizzik/client/renderer/SkizzikRenderer.java
@@ -4,6 +4,7 @@ import com.skizzium.projectapple.entity.boss.skizzik.Skizzik;
 import com.skizzium.projectapple.entity.boss.skizzik.client.model.SkizzikModel;
 import com.skizzium.projectapple.entity.boss.skizzik.client.renderer.layer.SkizzikGlowLayer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.core.BlockPos;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
 public class SkizzikRenderer extends GeoEntityRenderer<Skizzik> {
@@ -11,5 +12,11 @@ public class SkizzikRenderer extends GeoEntityRenderer<Skizzik> {
         super(renderer, new SkizzikModel());
         this.shadowRadius = 1.0F;
         //this.addLayer(new SkizzikGlowLayer(this));
+    }
+
+    @Override
+    protected int getBlockLightLevel(Skizzik entity, BlockPos pos) {
+        int stage = entity.stageManager.getCurrentStage().getStage().getId();
+        return stage <= 0 || stage >= 6 ? 0 : 15;
     }
 }
