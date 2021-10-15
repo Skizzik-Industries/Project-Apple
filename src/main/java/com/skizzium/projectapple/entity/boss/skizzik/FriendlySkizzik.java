a37008f8ec651f5c6ffc9ed346792e34947ad309
uSkizzik <58179002+uSkizzik@users.noreply.github.com>
2021-10-15 18:17:16 +0300

    Update FriendlySkizzik.java

--- src/main/java/com/skizzium/projectapple/entity/boss/skizzik/FriendlySkizzik.java
+++ src/main/java/com/skizzium/projectapple/entity/boss/skizzik/FriendlySkizzik.java
@@ -437,7 +437,7 @@ public class FriendlySkizzik extends Monster implements RangedAttackMob, IAnimat
     protected InteractionResult mobInteract(Player player, InteractionHand hand) {
         Item item = player.getMainHandItem().getItem();
         if (item instanceof Gem && PA_Tags.Items.SKIZZIK_BASE_GEMS.contains(item)) {
-            if (!this.getGems().contains(((Gem) item).getType())) {
+            if (!this.getGems().contains(((Gem) item).getType()) && !this.level.isClientSide) {
                 this.addGem(((Gem) item).getType());
             }
             return InteractionResult.sidedSuccess(!player.level.isClientSide);
